d6019d75036dd67465e482dbc2c10911a6a70344
Kevin Conway <kevinjacobconway@gmail.com>
2013-08-03 17:07:40 -0500

    Fix: Cleanup of files opened in setup.py
    Signed-off-by: Kevin Conway <kevinjacobconway@gmail.com>

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 from setuptools import setup
 
+with open('README.rst') as f:
+
+    long_description = f.read()
+
+with open('LICENSE') as f:
+
+    license = f.read()
+
 setup(
     name="pypm",
     version="0.0.1",
     description="A better way to manage and import dependencies.",
-    long_description=open('README.rst').read(),
+    long_description=long_description,
     author="Kevin Conway",
     author_email="kevinjacobconway@gmail.com",
     url="https://github.com/kevinconway/pypm",
     packages=['pypm'],
     scripts=['scripts/pypm'],
-    license=open('LICENSE').read(),
+    license=license,
     requires=[],
 )
